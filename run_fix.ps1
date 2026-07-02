d8f73f89a932f718992e54355b0e44bdd0e6334f
fengnian1236 <daijiexiao@gmail.com>
2026-07-02 14:07:57 +0800

    清理调试文件

--- run_fix.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location "C:\Users\daiji\meto-parts-library"
-$git = "D:\Qclaw\v0.2.30.594\resources\git\bin\git.exe"
-& $git checkout -- index.html
-Write-Output "Restored from git"
-D:\Qclaw\v0.2.30.594\resources\node\node.exe fix_edit_modal.js
--- run_fix2.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location "C:\Users\daiji\meto-parts-library"
-$git = "D:\Qclaw\v0.2.30.594\resources\git\bin\git.exe"
-& $git checkout -- index.html
-Write-Output "Restored"
-D:\Qclaw\v0.2.30.594\resources\node\node.exe find_pattern.js
